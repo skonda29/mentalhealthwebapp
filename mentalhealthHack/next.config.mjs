69b75da017d14198371948879a2b3a6f7787ffd0
Srinitya Kondapally <60264570+skonda29@users.noreply.github.com>
2025-04-19 11:19:10 -0700

    Delete mentalhealthHack directory

--- mentalhealthHack/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Helps highlight potential problems in your application
-    // You can add more configuration options as needed
-  };
-  
-  export default nextConfig;
-  
--- mentalhealthHack/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-import LandingPage from "./landingpage/page";
-
-export default function Home() {
-  return (
-    <LandingPage />
-  );
-}
